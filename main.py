2d842f1bc4561dc5689717999cc713944976729c
Anastasia <anastasiapolskaya@hotmail.com>
2025-08-29 21:53:31 +0000

    Complete step 4 initial testing

--- main.py
+++ main.py
@@ -86,7 +86,16 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters= {
+                "input": "clean_sample.csv:latest",
+                "test_size": config['modeling']['test_size'],
+                "random_seed": config['modeling']['random_seed'],
+                "stratify_by": config['modeling']['stratify_by']    
+                }
+            )
             ##################
             pass
 
@@ -101,7 +110,20 @@ def go(config: DictConfig):
             # step
 
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "output_artifact": "random_forest_export",
+                    "rf_config": rf_config,
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    
+                }
+            )
             ##################
 
             pass
