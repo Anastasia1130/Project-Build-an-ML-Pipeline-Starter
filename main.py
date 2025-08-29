04452981ce704def51acf7e91755ae55e01c59a9
Anastasia Polskaya <anastasiapolskaya@hotmail.com>
2025-08-29 08:54:34 +0000

    Complete data testing step 3 with validation checks

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
+            #################
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
                 "main",
@@ -63,11 +64,23 @@ def go(config: DictConfig):
                     "max_price": config['etl']['max_price'],
                 }
             )
+            #################
             pass
 
         if "data_check" in active_steps:
             ##################
-            # Implement here #
+            if "data_check" in active_steps:
+                _ = mlflow.run(
+                    os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                    "main",
+                    parameters={
+                        "csv": "clean_sample.csv:latest",
+                        "ref": "clean_sample.csv:reference",
+                        "kl_threshold": config["data_check"]["kl_threshold"],
+                        "min_price": config["etl"]["min_price"],
+                        "max_price": config["etl"]["max_price"],
+                    }
+            )
             ##################
             pass
 
